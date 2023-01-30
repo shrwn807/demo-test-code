9476aa53930ca76e64a0aad23e09cfacbeeff1a3
shrwn807 <shrwn807@gmail.com>
2023-01-31 07:48:52 +0800

    FX: Update and add comment for the test steps

--- src/test/java/fragrancex/demoTestCode.java
+++ src/test/java/fragrancex/demoTestCode.java
@@ -29,34 +29,34 @@ public class demoTestCode extends base {
 
     @Test
     public void fragrancexTest() throws IOException {
-
+        /* Go to Website */
         driver.get("https://www.fragrancex.com/");
         home = new homePage(driver);
         home.closeInitialDialog();
-
+        /* Store all links in an object */
         List<WebElement> allLinks = home.getAllLinks();
-
+        /* Log and Store all perfume names found under "Top Picks" */
         for (WebElement topPicksProduct : home.topPicks) {
             System.out.println(topPicksProduct.getText());
         }
-
+        /* Click on the 3rd product on Top Picks */
         for (WebElement topPick : home.selectTopPicksList(3)) {
             String selectedProductName = topPick.getText();
             System.out.println(selectedProductName);
             topPick.click();
             String productName = home.productName.getText();
+        /* Verify the Product name */
             assertTrue("Product Name not found", productName.contains(selectedProductName));
         }
-
-        //String cartCount = home.cartCount.getText();
         String cartCount = home.getCartCount();
+        /* Add the 2nd product variant to the bag */
         home.addToCartList2.get(2).click();
-        
         String updatedCartCount = home.getCartCount();
+        /* Verify Bag count update */
         Assert.assertEquals(Integer.parseInt(updatedCartCount), Integer.parseInt(cartCount.replaceAll("[^0-9]", "")) + 1);
         Select select = new Select(home.cartQuantity);
+        /* Update Quantity */
         select.selectByVisibleText("5");
-
     }
 
     public static void main(String[] args) {
